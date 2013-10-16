4ccc620084304fc9b9958dba16991c7735b21a06
nicolasalderete <nicolasalderete@gmail.com>
2013-10-16 00:30:52 -0300

    add prject

---
+++ desarrollo/tecno-server/src/main/java/ar/com/tecnosat/server/Main.java
@@ -0,0 +1,14 @@
+package ar.com.tecnosat.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		Logger log = LoggerFactory.getLogger(Main.class);
+		log.info("Inicion Main......................");
+	}
+
+}
